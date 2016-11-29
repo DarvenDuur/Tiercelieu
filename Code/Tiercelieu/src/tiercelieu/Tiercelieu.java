b45ecd1affe99f6188a04a4fb0f816923d8d66ed
Esteban Marquer <e.marquer1@gmail.com>
2016-11-29 11:18:40 +0100

    main

--- Code/Tiercelieu/src/tiercelieu/Tiercelieu.java
+++ Code/Tiercelieu/src/tiercelieu/Tiercelieu.java
@@ -15,7 +15,8 @@ public class Tiercelieu {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Control control=new Control();
+        control.play();
     }
     
 }
