3667d060857183c77640acccd079c230032cedd4
Esteban Marquer <e.marquer1@gmail.com>
2016-12-09 15:41:39 +0100

    Fin

--- Code/Tiercelieu/src/tiercelieu/Villager.java
+++ Code/Tiercelieu/src/tiercelieu/Villager.java
@@ -10,5 +10,29 @@ package tiercelieu;
  * @author emarq_000
  */
 public class Villager {
+    private boolean alive;
+    
+    /**
+     * 
+     */
+    public Villager() {
+        this.alive = true;
+    }
+    
+    
+    public boolean getAlive(){
+        return this.alive;
+    }
+
+    /**
+     * changes the value of alive
+     * @param newValue new boolean value for alive
+     */
+    protected void setAlive(boolean newValue){
+        this.alive = newValue;
+    }
+    protected void kill() {
+        this.setAlive(false);
+    }
     
 }
